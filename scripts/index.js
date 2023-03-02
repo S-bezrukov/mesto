ed563f586145ec7d3ddea01146926634a6b50e6f
Sergey Bezrukov <sb700sb@yandex.ru>
2023-03-02 14:55:00 +0300

    Удалить лишние шрифты

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+let page = document.querySelector('.page');
+let popup = page.querySelector('.popup');
+let popupImage = popup.querySelector('.popup__image');
+let mainContent = document.querySelector('.content');
+let profileEditButton = mainContent.querySelector('.profile__edit-button');
+let formElement = popup.querySelector('.form');
+let nameInput = formElement.querySelector('.form__input_type_name');
+let jobInput = formElement.querySelector('.form__input_type_job');
+let profileTitle = mainContent.querySelector('.profile__title');
+let profileDescription = mainContent.querySelector('.profile__description');
+nameInput.setAttribute('placeholder', profileTitle.textContent);
+jobInput.setAttribute('placeholder', profileDescription.textContent);
+
+function popupOpen() {
+  popup.classList.add('popup_opened');
+}
+
+function popupClose() {
+  popup.classList.remove('popup_opened');
+}
+
+profileEditButton.addEventListener('click', popupOpen);
+popupImage.addEventListener('click', popupClose);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileDescription.textContent = jobInput.value;
+    popupClose();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
