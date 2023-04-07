2d13a64b32e752d31260989361033d92c29ed958
Sergey Bezrukov <sb700sb@yandex.ru>
2023-04-07 17:37:47 +0300

    Выполнить Пр6

--- scripts/index.js
+++ scripts/index.js
@@ -22,6 +22,9 @@ const placeInputCard = formAddCard.querySelector('.form__input_type_place');
 const imageInputCard = formAddCard.querySelector('.form__input_type_photo');
 const nameInputProfile = formEditProfile.querySelector('.form__input_type_name');
 const jobInputProfile = formEditProfile.querySelector('.form__input_type_job');
+const formInputs = document.querySelectorAll('.form__input');
+const formButtonProfille = document.querySelector('.form__submit-btn_action_add');
+const formButtonCreate = document.querySelector('.form__submit-btn_action_create');
 const initialCards = [
   {
     name: 'Архыз',
@@ -52,6 +55,9 @@ const initialCards = [
 // Открываем попап
 const openPopup = function (popup) {
   popup.classList.add('popup_opened');
+  removeValidationErrors(formInputs, validationConfig);
+  document.addEventListener('keydown', closePopupEsc)
+  popup.addEventListener('click', closePopupOverlay)
 }
 
 // Закрываем попап
@@ -59,13 +65,30 @@ const closePopup = function (popup) {
   popup.classList.remove('popup_opened');
 }
 
+const closePopupEsc = function (evt) {
+  if (evt.key === 'Escape') {
+    const popupOpened = document.querySelector('.popup_opened')
+    closePopup(popupOpened);
+  }
+}
+
+const closePopupOverlay = function (evt) {
+  if (evt.target === evt.currentTarget) {
+    const popupOpened = document.querySelector('.popup_opened')
+    closePopup(popupOpened);
+  }
+}
+
 buttonEditProfile.addEventListener('click', function() {
+  enableButton (formButtonProfille, {inactiveButtonClass:validationConfig.inactiveButtonClass, activeButtonClass:validationConfig.activeButtonClass});
   nameInputProfile.value = profileTitle.textContent;
   jobInputProfile.value = profileDescription.textContent;
   openPopup(popupEditProfile); 
 })
 
 buttonAddCard.addEventListener('click', function() {
+  placeInputCard.value = "";
+  imageInputCard.value = "";
   openPopup(popupAddCard); 
 })
 
@@ -151,7 +174,94 @@ function handleFormSubmitAddCard (evt) {
   });
   elementsContainer.prepend(newCard);
   evt.target.reset();
+  disableButton(formButtonCreate, {inactiveButtonClass:validationConfig.inactiveButtonClass, activeButtonClass:validationConfig.activeButtonClass});
   closePopup(popupAddCard);
 }
 
 formAddCard.addEventListener('submit', handleFormSubmitAddCard);
+
+// Валидация форм
+const validationConfig = {
+  formSelector: '.form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__submit-btn',
+  inactiveButtonClass: 'form__submit-btn_action_disabled',
+  activeButtonClass: 'form__submit-btn_action_enabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}
+
+//эта функция запускает валидацию
+const enableValidation = ({formSelector, ...rest}) => {   
+	const forms = Array.from(document.querySelectorAll(formSelector))
+	forms.forEach(form => {
+		form.addEventListener('submit', (evt) => {
+		evt.preventDefault();
+		})
+		setEventListeners(form, rest)
+	})
+}
+
+const setEventListeners = (formToValidate, { inputSelector, submitButtonSelector, ...rest }) => {
+	const formInputs = Array.from(formToValidate.querySelectorAll(inputSelector)) // с начало выполниться то, что в скобках, найдутся псевдомассив со всеми инпутами фомы и Array.from преобразует их в массив инпутов
+	const formButton = formToValidate.querySelector(submitButtonSelector) //получаем ссылку на кнопку
+	disableButton(formButton, rest) // сделаем кнопку не активной, когда страница только загрузилась и в поля данные ещё не вводились. 
+	formInputs.forEach(input => { //теперь пройдёмся по этому массиву циклом и с каждым элемнтом выполним деёствие, берём инпут и накладываем на него слушатель
+		input.addEventListener('input', () => {
+			checkInputValidity(input, rest)
+			if (hasInvalidInput(formInputs)) {
+				disableButton(formButton, rest)  // сделать эту кнопку не валидной
+			} else {
+				enableButton(formButton, rest) // сделать эту кнопку валидной
+			}
+		})
+	})
+}
+
+const checkInputValidity = (input, {inputErrorClass, errorClass}) => {
+	if (input.checkValidity()) {      //Если поле отдаст true, значит поле прошло валидацию
+		hideInputError(input, {inputErrorClass, errorClass})
+	} else {
+		showInputError(input, {inputErrorClass, errorClass})
+	}
+}
+
+const showInputError = (input, {inputErrorClass, errorClass}) => {
+  const inputError = document.querySelector(`#${input.id}`)
+  inputError.classList.add(inputErrorClass);
+  const errorMessage = document.querySelector(`#${input.id}-error`)
+  errorMessage.textContent = input.validationMessage;
+  errorMessage.classList.add(errorClass);
+}
+
+const hideInputError = (input, {inputErrorClass, errorClass}) => {
+  const inputError = document.querySelector(`#${input.id}`)
+  inputError.classList.remove(inputErrorClass);
+  const errorMessage = document.querySelector(`#${input.id}-error`)
+  errorMessage.classList.remove(errorClass);
+  errorMessage.textContent = '';
+}
+
+const hasInvalidInput = (formInputs) => {
+	return formInputs.some(item => !item.validity.valid) // в эту функцию передаём массив formInputs и проверяем, если хоть одно поле не валидно то получаем false иначе true
+}
+
+const enableButton = (button, {inactiveButtonClass, activeButtonClass}) => {
+	button.classList.remove(inactiveButtonClass)
+	button.classList.add(activeButtonClass)
+	button.removeAttribute('disabled')	
+}
+
+const disableButton = (button, {inactiveButtonClass, activeButtonClass}) => {
+	button.classList.add(inactiveButtonClass)
+	button.classList.remove(activeButtonClass)
+  button.setAttribute('disabled', true)
+}
+
+enableValidation (validationConfig) //вызываем функцию
+
+const removeValidationErrors = function(inputs, validationConfig) {
+  inputs.forEach(input => {
+    hideInputError(input, validationConfig)
+})
+}
